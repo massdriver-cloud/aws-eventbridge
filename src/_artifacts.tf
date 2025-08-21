324cb6dcf3f9f04c562954b9baa1273b5d5e04f8
chrisghill <chris@massdriver.cloud>
2025-08-21 13:39:25 -0600

    remove deprecated field

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -1,7 +1,6 @@
 resource "massdriver_artifact" "infrastructure" {
-  field                = "eventbridge"
-  provider_resource_id = aws_cloudwatch_event_bus.main.arn
-  name                 = "EventBridge ${var.md_metadata.name_prefix}"
+  field    = "eventbridge"
+  name     = "EventBridge ${var.md_metadata.name_prefix}"
   artifact = jsonencode(
     {
       data = {
